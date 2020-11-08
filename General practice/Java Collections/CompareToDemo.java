d95f228c7dfdcb958e200b164a42f7d0fb3272e1
mayankpshahi <mayankpshahi239@gmail.com>
2020-11-09 01:34:45 +0530

    vscommit

--- General practice/Java Collections/CompareToDemo.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class CompareToDemo{
-	public static void main(String[] args) {
-		System.out.println("A".compareTo("Z"));//-ve
-		System.out.println("Z".compareTo("K"));//+ve
-		System.out.println("A".compareTo("A"));//0
-		System.out.println("A".compareTo(null));//RE:NPE
-	}
-}
