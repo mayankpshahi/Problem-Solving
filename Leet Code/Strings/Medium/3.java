9c9defd06015d5e677d66061510987600795a956
mayankpshahi <mayankpshahi239@gmail.com>
2020-11-10 00:15:50 +0530

    108

---
+++ Leet Code/Strings/Medium/3.java
@@ -0,0 +1,19 @@
+//3. Longest Substring Without Repeating Characters
+
+
+//o(n)
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        
+        int result = 0;
+        int[] cache = new int[256];
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            j = (cache[s.charAt(i)] > 0) ? Math.max(j, cache[s.charAt(i)]) : j;
+            cache[s.charAt(i)] = i + 1;
+            result = Math.max(result, i - j + 1);
+        }
+        return result;
+        
+        
+    }
+}
