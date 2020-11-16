05a00e57fa309959c195b3e092ab592ba24d2a79
mayankpshahi <mayankpshahi239@gmail.com>
2020-11-16 17:37:43 +0530

    131

---
+++ Leet Code/Tree/Medium/94.java
@@ -0,0 +1,75 @@
+//94. Binary Tree Inorder Traversal
+
+//Recursive O(n) time and space
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> l  =  new ArrayList<Integer>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        
+        
+        if (root == null) return l;
+        
+        inorderTraversal(root.left);
+        
+        l.add(root.val);
+        
+        return inorderTraversal(root.right);
+        
+        
+        
+    }
+}
+
+
+//Iterative Solution O(n) time and space
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List < Integer > res = new ArrayList < > ();
+        Stack < TreeNode > stack = new Stack < > ();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+        return res;
+        
+        
+        
+    }
+}
