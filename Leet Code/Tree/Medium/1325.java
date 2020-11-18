4262a23954d51f26ebcf48289a8776a35cb0ef8b
mayankpshahi <mayankpshahi239@gmail.com>
2020-11-18 23:58:28 +0530

    154

---
+++ Leet Code/Tree/Medium/1325.java
@@ -0,0 +1,33 @@
+//1325. Delete Leaves With a Given Value
+
+//Recursive
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if (root == null) return null;
+                
+        root.left = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
+     
+        if (root.left == null && root.right == null && root.val == target)
+        {
+             return null;
+        }
+       else  return root;
+        
+    }
+}
