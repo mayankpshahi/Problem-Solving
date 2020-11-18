c95bfce5f29000775bdb3d015fd588a56742d543
mayankpshahi <mayankpshahi239@gmail.com>
2020-11-18 23:17:55 +0530

    152

---
+++ Leet Code/Tree/Medium/894.java
@@ -0,0 +1,85 @@
+//894. All Possible Full Binary Trees
+
+//Recursive O(N^2)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> allPossibleFBT(int N) {
+        List<TreeNode> res=new ArrayList<>();
+        
+        if(N==1){
+			TreeNode root=new TreeNode(0);
+			res.add(root);
+		}
+        for(int i=1;i<N;i+=2){
+			for(TreeNode left : allPossibleFBT(i)){
+				for(TreeNode right : allPossibleFBT(N-i-1)){
+					TreeNode root=new TreeNode(0);
+					root.left=left;
+					root.right=right;
+					res.add(root);                    
+				}
+			}
+		}
+		return res;  
+        
+    }
+}
+
+
+//More Efficient Solution using HashMap O(2^N)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<Integer, List<TreeNode>> memo = new HashMap();
+
+    public List<TreeNode> allPossibleFBT(int N) {
+        if (!memo.containsKey(N)) {
+            List<TreeNode> ans = new LinkedList();
+            if (N == 1) {
+                ans.add(new TreeNode(0));
+            } else if (N % 2 == 1) {
+                for (int x = 0; x < N; ++x) {
+                    int y = N - 1 - x;
+                    for (TreeNode left: allPossibleFBT(x))
+                        for (TreeNode right: allPossibleFBT(y)) {
+                            TreeNode bns = new TreeNode(0);
+                            bns.left = left;
+                            bns.right = right;
+                            ans.add(bns);
+                        }
+                }
+            }
+            memo.put(N, ans);
+        }
+
+        return memo.get(N);
+    }
+}
