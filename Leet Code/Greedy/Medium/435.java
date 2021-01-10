40e11ab672de03ff2a392104881a1767f5b210d8
Mayank Pratap Shahi <mayankpshahi239@gmail.com>
2021-01-10 21:40:06 +0530

    212

---
+++ Leet Code/Greedy/Medium/435.java
@@ -0,0 +1,26 @@
+//435. Non-overlapping Intervals
+
+
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if(intervals==null || intervals.length==0) return 0;
+        
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[1], b[1]));
+        
+        int count  = 0;
+        
+        int lastEnd = intervals[0][1];
+        
+        for(int i = 1;  i < intervals.length; i++){
+            
+            if(intervals[i][0] < lastEnd)
+                count++;
+            else
+                lastEnd = intervals[i][1];
+            
+        }
+        
+        return count;
+        
+    }
+}
