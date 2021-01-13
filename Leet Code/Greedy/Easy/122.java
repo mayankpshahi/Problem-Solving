238ac88fc3c28a8828b8723300076dc6c002ff86
Mayank Pratap Shahi <mayankpshahi239@gmail.com>
2021-01-13 16:43:37 +0530

    218

---
+++ Leet Code/Greedy/Easy/122.java
@@ -0,0 +1,13 @@
+//122. Best Time to Buy and Sell Stock II
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+               int profit=0;         
+             for(int i=1;i<prices.length;i++){             
+                  profit+=Math.max(0,prices[i]-prices[i-1]);         
+             }        
+             return profit;     
+    
+    }
+}
