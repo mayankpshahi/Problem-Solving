86d1c39eff9f042edc45caefba69a5193995aeaf
Mayank Pratap Shahi <mayankpshahi239@gmail.com>
2021-01-22 19:28:24 +0530

    241

---
+++ Leet Code/Dynamic Programming/Easy/121.java
@@ -0,0 +1,15 @@
+//121. Best Time to Buy and Sell Stock
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0,min=prices[0];
+        for(int i = 1;i<prices.length;i++){
+                min =  Math.min(min,prices[i]);
+                profit =  Math.max(profit,prices[i]-min);
+            
+        }
+        
+        
+        return profit;
+    }
+}
