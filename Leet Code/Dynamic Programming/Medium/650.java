a73dc9e1df47e3679c6b984d943f1d3b07f54ef2
Mayank Pratap Shahi <mayankpshahi239@gmail.com>
2021-01-28 19:14:19 +0530

    258

--- Leet Code/Dynamic Programming/Medium/650.java
+++ Leet Code/Dynamic Programming/Medium/650.java
@@ -27,3 +27,20 @@ class Solution {
         return dp[n];
     }
 }
+
+
+//Prime Factorization
+
+class Solution {
+    public int minSteps(int n) {
+        int ans = 0, d = 2;
+        while (n > 1) {
+            while (n % d == 0) {
+                ans += d;
+                n /= d;
+            }
+            d++;
+        }
+        return ans;
+    }
+}
