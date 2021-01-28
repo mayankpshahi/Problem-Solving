b2d31e86edcad7bff9b48909977a0609ec0105a9
Mayank Pratap Shahi <mayankpshahi239@gmail.com>
2021-01-28 19:03:04 +0530

    258

---
+++ Leet Code/Dynamic Programming/Medium/650.java
@@ -0,0 +1,29 @@
+//650. 2 Keys Keyboard
+
+
+
+class Solution {
+    public int minSteps(int n) {
+        
+        if(n==1) return 0;
+        else if(n==2) return 2;
+        
+        int dp [] =  new int[n+1];
+        
+        dp[1] = 0; dp[2] = 2;
+        
+        for(int i=3;i<=n;i++){
+          dp[i] = i;
+          int j= i/2;
+          while(j>=1){
+              if(i%j==0){
+                  dp[i]= Math.min(dp[i],dp[j]+(i/j));
+                  break;
+              }  
+            j--;
+          }
+        }
+        
+        return dp[n];
+    }
+}
