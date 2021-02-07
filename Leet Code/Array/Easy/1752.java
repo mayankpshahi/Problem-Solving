88d135c88069a4039189ea2e0c88f25fa76ef54b
Mayank Pratap Shahi <mayankpshahi239@gmail.com>
2021-02-07 16:42:34 +0530

    271

---
+++ Leet Code/Array/Easy/1752.java
@@ -0,0 +1,19 @@
+
+//1752. Check if Array Is Sorted and Rotated
+
+class Solution {
+    
+   
+    public boolean check(int[] nums) {
+     int count=0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] > nums[(i + 1) % nums.length]) {
+                count++;
+            }
+            if (count > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
