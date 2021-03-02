caaba49d218bd26d70e13bbdc74f645f07412866
Mayank Pratap Shahi <mayankpshahi239@gmail.com>
2021-03-02 14:17:23 +0530

    282

--- Leet Code/Dynamic Programming/Medium/322.java
+++ Leet Code/Dynamic Programming/Medium/322.java
@@ -23,3 +23,40 @@ class Solution {
     return mincoins[n][amount]>10000?-1:mincoins[n][amount];
     }
 }
+
+//Another solution.
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+     int M =  coins.length;
+     int V = amount;
+     int t[][] =  new int[M+1][V+1];
+         int i=0,j=0;
+        for(j=0;j<V+1;j++)
+          t[i][j]=Integer.MAX_VALUE -1;
+          
+        j=0;
+        for(i=1;i<M+1;i++)
+          t[i][j]=0;
+          
+        i=1;
+         for(j=1;j<V+1;j++){
+             if(j%coins[0]==0)
+              t[i][j] = j/coins[0];
+             else
+              t[i][j]=Integer.MAX_VALUE -1;
+         }
+          
+         
+        i=0;j=0;
+        for(i=2;i<M+1;i++){
+            for(j=1;j<V+1;j++){
+                if(coins[i-1]<=j)
+                 t[i][j] = Math.min(1+t[i][j-coins[i-1]] , t[i-1][j]);
+                else
+                  t[i][j]=t[i-1][j];
+            }
+        }
+         
+        return t[M][V]!=Integer.MAX_VALUE-1?t[M][V]:-1 ;
+    }
+}
