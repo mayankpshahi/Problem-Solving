96c98c4caa9bf5c3f8b40a91aff8e6c580883ed5
mayankpshahi <mayankpshahi239@gmail.com>
2021-04-06 16:31:51 +0530

    375

---
+++ Leet Code/Stack/Hard/32. Longest Valid Parentheses.java
@@ -0,0 +1,46 @@
+
+
+//32. Longest Valid Parentheses
+
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        char arr[] =  s.toCharArray();
+        
+        int n =  arr.length;
+        
+        int len  = 0;
+        
+        Stack<Integer> st =  new Stack<>();
+        
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == '(') st.push(i);
+            else {
+                if (!st.isEmpty()) {
+                    if (arr[st.peek()] == '(') st.pop();
+                    else st.push(i);
+                }
+                else st.push(i);
+            }
+        }
+        
+        if (st.isEmpty()) return n;
+        else {
+            int end = n, start = 0;
+            while (!st.isEmpty()) {
+                start = st.peek(); 
+                st.pop();
+                int validlen = end-start-1;
+                len = len > validlen ? len : validlen;
+                end = start;
+            }
+              len = len > end ? len : end;
+        }
+        
+        
+        return len;
+        
+        
+        
+    }
+}
