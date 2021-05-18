defc8d89120e24f6b115deb2faa991bfd8a65abe
mayankpshahi <mayankpshahi239@gmail.com>
2021-05-18 21:09:08 +0530

    421

---
+++ Leet Code/Array/Easy/66. Plus One.java
@@ -0,0 +1,25 @@
+
+
+//66. Plus One
+
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        
+        for(int i= n-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            
+            digits[i] = 0;
+        }
+        
+        int newval[] = new int[n+1];
+        newval[0] = 1;
+        
+        return newval;
+        
+    }
+}
