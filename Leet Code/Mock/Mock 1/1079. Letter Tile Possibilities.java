07918b49362f56c322eed5f6fe30b583445da26b
mayankpshahi <mayankpshahi239@gmail.com>
2021-05-28 20:25:36 +0530

    427

---
+++ Leet Code/Mock/Mock 1/1079. Letter Tile Possibilities.java
@@ -0,0 +1,35 @@
+
+
+\\1079. Letter Tile Possibilities
+
+
+class Solution {
+    private int count =0;
+    public  int numTilePossibilities(String tiles) {
+        int []freq =  new int[26];
+        Arrays.fill(freq,0);
+        
+        for(int i=0;i<tiles.length();i++) freq[tiles.charAt(i) - 'A']++;
+        
+        countseq(freq);
+        
+        return count;
+        
+        
+    }
+    
+    private void countseq(int [] freq){
+    
+    for(int i=0;i<26;i++){
+      if(freq[i]>0){
+           count++;
+          freq[i]--;
+         
+          
+          countseq(freq);
+          freq[i]++;
+      }  
+    }
+        
+    }
+}
