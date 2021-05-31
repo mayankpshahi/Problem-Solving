bfe759735eb44f00a12a721f7c9b961803ba249f
mayankpshahi <mayankpshahi239@gmail.com>
2021-05-31 22:55:11 +0530

    456

---
+++ Leet Code/Strings/Easy/242. Valid Anagram.java
@@ -0,0 +1,20 @@
+
+
+
+//242. Valid Anagram
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+            return false;
+        
+        char [] s1 = s.toCharArray();
+        char [] s2 = t.toCharArray();
+        
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+        
+        return Arrays.equals(s1,s2);
+        
+    }
+}
