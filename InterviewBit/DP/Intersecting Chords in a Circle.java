dd3f32b9c6986c2adbd528a84e77813dfa904f3e
mayankpshahi <mayankpshahi239@gmail.com>
2021-06-01 18:16:14 +0530

    466

--- InterviewBit/DP/Intersecting Chords in a Circle.java
+++ InterviewBit/DP/Intersecting Chords in a Circle.java
@@ -48,3 +48,20 @@ public class Solution {
 }
 
 
+//catalan No.
+
+
+int Solution::chordCnt(int n) {
+    
+    vector<long int> dp(n+1,0);
+    dp[0]=1;
+    dp[1]=1;
+    for(int i=2;i<=n;i++)
+    {
+        for(int x=0,y=i-1;x<i;x++,y--)
+        {
+            dp[i]=(dp[i] + (dp[x]*dp[y]))%1000000007;
+        }
+    }
+    return dp[n]%1000000007;
+}
