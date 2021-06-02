53fa69e8142e0ffbd927d0e5198bde4fd4cea666
mayankpshahi <mayankpshahi239@gmail.com>
2021-06-02 17:01:09 +0530

    475

---
+++ Leet Code/Dynamic Programming/Hard/115. Distinct Subsequences.java
@@ -0,0 +1,38 @@
+
+
+//115. Distinct Subsequences
+
+
+class Solution {
+    int t[][];
+    public int numDistinct(String s1, String s2) {
+        t = new int[s2.length()+1][s1.length()+1];
+        
+        
+        int count = distinctSubsequence(s1,s2);
+        
+       
+        return count;
+        
+    }
+    public int distinctSubsequence(String str1, String str2)
+    {   
+        int p = str1.length();
+        int q = str2.length();
+        // code here
+        
+        
+    for(int j=0; j<p+1; j++) {
+        t[0][j] = 1;
+    }
+       
+    for(int i=1;i<q+1;i++){
+       for(int j=1;j<p+1;j++){
+            if(str2.charAt(i-1) == str1.charAt(j-1))  t[i][j] = t[i-1][j-1] + t[i][j-1];
+            else t[i][j] = t[i][j-1];
+       } 
+    } 
+    
+    return t[q][p];
+    }
+}
