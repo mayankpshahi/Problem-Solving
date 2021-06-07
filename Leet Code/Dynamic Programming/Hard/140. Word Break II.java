1871e77a6b7e5a8fd1a11c26953dce59c9badaf2
mayankpshahi <mayankpshahi239@gmail.com>
2021-06-07 16:01:41 +0530

    491

--- Leet Code/Dynamic Programming/Hard/140. Word Break II.java
+++ Leet Code/Dynamic Programming/Hard/140. Word Break II.java
@@ -40,3 +40,42 @@ class Solution {
         
     }
 }
+
+
+//Memoization
+
+
+
+class Solution {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        
+        HashMap<Integer, List<String>> hm = new HashMap<>();
+        HashSet<String> dict =  new HashSet<>(wordDict);
+        
+        return wordBreakHelper(s,dict,0,hm);
+        
+    }
+    private List<String> wordBreakHelper(String s,HashSet<String> dict,int start, HashMap<Integer, List<String>> hm){
+        
+        if(hm.containsKey(start))return hm.get(start);
+        
+        List<String> valid = new ArrayList<>();
+        
+        if(start==s.length()) valid.add("");
+        
+        for(int end = start+1;end<=s.length();end++){
+            String prefix = s.substring(start,end);
+            if(dict.contains(prefix)){
+                 List<String> suffix = wordBreakHelper(s,dict,end,hm);
+                
+                for(String word:suffix){
+                    valid.add(prefix + (word.equals("")?"":" ")+word);
+                }
+            }
+        }
+        
+        hm.put(start,valid);
+        return valid;
+        
+    }
+}
