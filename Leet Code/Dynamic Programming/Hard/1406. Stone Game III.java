91e92a9d18ff095d6caf632a557bd8d6c01d1dc3
mayankpshahi <mayankpshahi239@gmail.com>
2021-06-14 20:47:08 +0530

    523

--- Leet Code/Dynamic Programming/Hard/1406. Stone Game III.java
+++ Leet Code/Dynamic Programming/Hard/1406. Stone Game III.java
@@ -4,7 +4,7 @@
 
 //1406. Stone Game III
 
-
+//Memoization
 class Solution {
     int t[];
     public String stoneGameIII(int[] stoneValue) {
@@ -26,4 +26,43 @@ class Solution {
              return t[i]=res;
         }
     }
+}
+
+
+
+//Top Down
+
+
+public String stoneGameIII(int[] A) {
+    int n = A.length, dp[] = new int[n+1];
+    for (int i = n - 1; i >= 0; --i) {
+        dp[i] = Integer.MIN_VALUE;
+        for (int k = 0, take = 0; k < 3 && i + k < n; ++k) {
+            take += A[i + k];
+            dp[i] = Math.max(dp[i], take - dp[i + k + 1]);
+        }
+    }
+    if (dp[0] > 0) return "Alice";
+    if (dp[0] < 0) return "Bob";
+    return "Tie";
+}
+
+
+
+
+//O(1) space
+
+
+public String stoneGameIII(int[] A) {
+    int n = A.length, dp[] = new int[4];
+    for (int i = n - 1; i >= 0; --i) {
+        dp[i % 4] = Integer.MIN_VALUE;
+        for (int k = 0, take = 0; k < 3 && i + k < n; ++k) {
+            take += A[i + k];
+            dp[i % 4] = Math.max(dp[i % 4], take - dp[(i + k + 1) % 4]);
+        }
+    }
+    if (dp[0] > 0) return "Alice";
+    if (dp[0] < 0) return "Bob";
+    return "Tie";
 }
