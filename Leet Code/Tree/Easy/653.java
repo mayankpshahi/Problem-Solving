773d3f714cd59d3c825c2554a47d673d866f35ad
mayankpshahi <mayankpshahi239@gmail.com>
2021-06-19 21:42:22 +0530

    559

--- Leet Code/Tree/Easy/653.java
+++ Leet Code/Tree/Easy/653.java
@@ -36,3 +36,41 @@ class Solution {
         inorder(root.right,nums);
     }
 }
+
+
+
+//Other solution
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    HashSet<Integer> set;
+    public boolean findTarget(TreeNode root, int k) {
+        set = new HashSet<Integer>();
+        return findTargetPair(root, k);
+    }
+     private boolean findTargetPair(TreeNode root, int k){
+        if(root == null)
+            return false;
+        if(set.contains(k - root.val))
+            return true;
+        
+        set.add(root.val);
+        return findTargetPair(root.left, k) || findTargetPair(root.right,  k);
+    }
+}
