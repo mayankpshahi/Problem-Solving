53362caeb6a393170d6e1d3618da65aa777d9ab6
mayankpshahi <mayankpshahi239@gmail.com>
2021-06-21 15:16:15 +0530

    562

---
+++ Leet Code/Tree/Medium/236. Lowest Common Ancestor of a Binary Tree.java
@@ -0,0 +1,27 @@
+
+
+
+
+//236. Lowest Common Ancestor of a Binary Tree
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null||root==p||root==q) return root;
+        TreeNode l =  lowestCommonAncestor(root.left, p, q);
+        TreeNode r =  lowestCommonAncestor(root.right, p, q);
+        if(l!=null && r!=null) return root;
+        return l!=null?l:r;
+        
+    }
+}
