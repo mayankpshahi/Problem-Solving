192e666a4853a0d62cdfab62755de9f82276a812
mayankpshahi <mayankpshahi239@gmail.com>
2021-06-23 15:48:39 +0530

    566

---
+++ Leet Code/Graph/Medium/1791. Find Center of Star Graph.java
@@ -0,0 +1,24 @@
+
+
+
+
+
+//1791. Find Center of Star Graph
+
+
+
+class Solution {
+    public int findCenter(int[][] edges) {
+        Set<Integer> set = new HashSet<>();
+        
+        for(int[]e:edges){
+            if(set.contains(e[0])) return e[0];
+            else set.add(e[0]);
+            if(set.contains(e[1])) return e[1];
+            else set.add(e[1]);
+        }
+        
+        return -1;
+        
+    }
+}
