acf8bfec4fcabff7ae1bb550676a7207c5839cdb
mayankpshahi <mayankpshahi239@gmail.com>
2021-06-26 18:03:49 +0530

    576

---
+++ InterviewBit/Graph/Convert Sorted List to Binary Search Tree.java
@@ -0,0 +1,45 @@
+
+
+
+
+
+//Convert Sorted List to Binary Search Tree
+
+
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+         if(head==null) return null;
+       return createBST(head,null);
+    }
+    public TreeNode createBST(ListNode head, ListNode tail){
+    ListNode slow = head;
+    ListNode fast = head;
+    if(head==tail) return null;
+    
+    while(fast!=tail&&fast.next!=tail){
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    TreeNode thead = new TreeNode(slow.val);
+    thead.left = createBST(head,slow);
+    thead.right =  createBST(slow.next,tail);
+    return thead;
+}
+}
---
+++ Leet Code/Tree/Medium/109. Convert Sorted List to Binary Search Tree.java
@@ -0,0 +1,53 @@
+
+
+
+//109. Convert Sorted List to Binary Search Tree
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head==null) return null;
+       return createBST(head,null);
+        
+    }
+    public TreeNode createBST(ListNode head, ListNode tail){
+    ListNode slow = head;
+    ListNode fast = head;
+    if(head==tail) return null;
+    
+    while(fast!=tail&&fast.next!=tail){
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    TreeNode thead = new TreeNode(slow.val);
+    thead.left = createBST(head,slow);
+    thead.right =  createBST(slow.next,tail);
+    return thead;
+}
+}
