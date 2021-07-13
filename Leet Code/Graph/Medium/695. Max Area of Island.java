f52aae2647569be010ff87a90005f320663339a7
mayankpshahi <mayankpshahi239@gmail.com>
2021-07-13 21:22:11 +0530

    603

---
+++ Leet Code/Graph/Medium/695. Max Area of Island.java
@@ -0,0 +1,31 @@
+
+
+
+
+
+//695. Max Area of Island
+
+
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    maxArea = Math.max(maxArea,dfs(grid,i,j));
+                }
+            }
+        }
+        
+        return maxArea;
+        
+    }
+    private int dfs(int[][]grid,int i,int j){
+        if(i>=0 && i<grid.length && j>=0 && j<grid[0].length && grid[i][j]==1){
+            grid[i][j]=0;
+            
+            return 1 + dfs(grid,i+1,j) + dfs(grid,i-1,j) + dfs(grid,i,j+1)+dfs(grid,i,j-1);
+        }
+        return 0;
+    }
+}
