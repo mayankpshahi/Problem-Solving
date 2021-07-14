ba5a108b84f58bf16fc3f0b881b6e203a4faf54a
mayankpshahi <mayankpshahi239@gmail.com>
2021-07-14 16:18:20 +0530

    607

---
+++ Leet Code/Strings/Easy/796. Rotate String.java
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+//796. Rotate String
+
+
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        return s.length() == goal.length() && (s + s).contains(goal);
+        
+    }
+}
