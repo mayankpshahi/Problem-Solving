be34bea3befce11867fdb79c9ddb19b1c484e4fd
mayankpshahi <mayankpshahi239@gmail.com>
2021-07-15 12:55:05 +0530

    608

--- Leet Code/Strings/Easy/459. Repeated Substring Pattern.java
+++ Leet Code/Strings/Easy/459. Repeated Substring Pattern.java
@@ -22,3 +22,31 @@ class Solution {
 	return false;
     }
 }
+
+
+
+
+//KMP algorithm Solution
+
+
+
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+     int n =  s.length();
+        
+    int lps [] =  new int[n];
+        
+    for(int i=1;i<n;i++){
+        int j= lps[i-1];
+        while(j>0 && s.charAt(i)!=s.charAt(j)) j=lps[j-1];
+        
+        if(s.charAt(i)==s.charAt(j)) j++;
+        lps[i]=j;
+    }
+        
+        
+    int l= lps[n-1];
+    
+    return (l!=0) && (n%(n-l)==0);
+    }
+}
