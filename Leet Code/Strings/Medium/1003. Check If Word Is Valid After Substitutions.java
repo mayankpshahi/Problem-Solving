c75d92e0d005ce5e2c8244824017ea53f3bad275
mayankpshahi <mayankpshahi239@gmail.com>
2021-07-20 00:01:57 +0530

    619

---
+++ Leet Code/Strings/Medium/1003. Check If Word Is Valid After Substitutions.java
@@ -0,0 +1,23 @@
+
+
+
+
+//1003. Check If Word Is Valid After Substitutions
+
+
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c: s.toCharArray()) {
+            if (c == 'c') {
+                if (stack.isEmpty() || stack.pop() != 'b') return false;
+                if (stack.isEmpty() || stack.pop() != 'a') return false;
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+        
+    }
+}
