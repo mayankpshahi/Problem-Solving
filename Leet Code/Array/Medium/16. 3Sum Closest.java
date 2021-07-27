e3ac7bd846d73808c3723be5c6c8ac917a821a2d
mayankpshahi <mayankpshahi239@gmail.com>
2021-07-27 17:55:48 +0530

    643

---
+++ Leet Code/Array/Medium/16. 3Sum Closest.java
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+//16. 3Sum Closest
+
+
+class Solution {
+    public int threeSumClosest(int[] num, int target) {
+        int res = num[0] + num[1] + num[num.length - 1];
+        Arrays.sort(num);
+        for (int i = 0; i < num.length - 2; i++) {
+            int start = i + 1, end = num.length - 1;
+            while (start < end) {
+                int sum = num[i] + num[start] + num[end];
+                if (sum > target) {
+                    end--;
+                } else {
+                    start++;
+                }
+                if (Math.abs(sum - target) < Math.abs(res - target)) {
+                    res = sum;
+                }
+            }
+        }
+        return res;
+        
+    }
+}
